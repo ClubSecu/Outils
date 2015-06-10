cf74535b5bca34fe185006924c3192fed2bdf723
Nomekrax <nomekrax@test.com>
2015-06-10 11:24:08 +0200

    Modifying arborescence tree by adding folder for each tool (see Best Pratices)

---
+++ HexToLEndian/hextolendian.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+""" 
+    Author : Nomekrax
+    Date : 10/06/15
+    Informations : Python 2.7.3
+"""
+
+import struct
+import os
+import sys
+import string
+
+if len(sys.argv) is not 2:
+    print "Usage : ", sys.argv[0], " [0x00000000]"
+    sys.exit()
+
+result = struct.pack("<I",int(sys.argv[1],16)).encode('string-escape')
+
+print(result)
+sys.exit()
